0015f2dd08131a1dd521370efb33f17c187a07a1
Marsh1100 <margie.b.agu17@gmail.com>
2023-06-07 21:58:28 -0500

    feat: :construction: Proyecto aerolíneas

---
+++ travelExp.js
@@ -0,0 +1,11 @@
+
+// Botones
+const botonClientes = document.getElementById('clientes');
+const botonRutas = document.getElementById('rutas');
+const botonCompraTiquete = document.getElementById('compra-tiquetes');
+const botonPuntos = document.getElementById('puntos');
+
+
+botonClientes.addEventListener('click',function(){
+    
+});
